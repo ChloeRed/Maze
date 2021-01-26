f8ce0e76ad37c3feeb6e4ad7c2b41cc13dbbb95c
ChloeRed <48622771+ChloeRed@users.noreply.github.com>
2021-01-26 11:44:39 -0500

    Add files via upload

---
+++ README.cs
@@ -0,0 +1,43 @@
+﻿/*Chloé Reddy
+ * 260786570
+ * Assignment1
+ * COMP521
+ *
+ *Hello! Here is my game, I hope you enjoy. I know it is not 100% correct
+ * (I'll explain my troubles in the challenges), but I wanted something that was at least
+ * playable.
+ *
+ * Algorithm: I used DFS to create my maze. I first created a grid and then used the algorithm to create a perfect maze.
+ * I was not, however, able to create an exit or make it dynamic. It is random, as different walls are broken down each time
+ * to create a new maze, but it is only generated once per maze. I was also not able to track the solution. But it is a solvable maze!
+ *
+ * The only assest I used was the Standard Asset and that was for the FPS player. It saved me a lot of time when creating my character
+ * and allowed for me to advance a bit on the assignment. The way I learned how to do mainly everything for this assignment was through
+ * watching many YouTube videos. I have Flat Tutorials to thank for my somewhat functional maze generation and other youtubers for the
+ * object instatiation, etc.
+ *
+ * As for what was challenging, I think my biggest problem was unfamiliarity with the software/language. I did not know C# before this
+ * so on top of learning how to use Unity, I had to learn C# as well. That being said, learning how to navigate Unity was probably the tougher
+ * of the two (though tutorials really helped!) I also had overcomplicated the project in my head. I had spent a couple days trying to create
+ * this complicated terrain using algorithms we learned in class before I saw a TA and realized that I could just make it a flat plane.
+ * I found the first part of this project to be rather enjoyable. I usually hate my COMP assignments because it's hard to see immediate results sometimes.
+ * But with Unity and this project, I was making something I could interact with immediately.
+ * When I got to the maze, I spent a couple days trying to even generate a maze. But, as it was material we had gone over in class, it was more familiar,
+ * unlike the rest of the project. When I finally had done it, I had a tough time tracking the player insidthe maze.
+ * Everything I tried kept crashing the program. Rather than turn in corrupt code, I wanted to turn in something that I would feel better
+ * taking credit for, which is why this project is incomplete. I think if I had
+ * a) not spent so much time in the beginning overcomplicating the project
+ * b) gotten more time before the project to explore unity and learn some basic skills,
+ * I think I could've achieved more.
+ *
+ * But, I really enjoyed this project and am enjoying the class. I will maybe just take more advantage of office hours next time!
+ *
+ * Thanks,
+ *
+ * Chloé 
+ *
+ *
+ *
+ *
+ */
+ 
